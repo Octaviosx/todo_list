0d43a82afaac89c5127c2b060d99ad2db5e2224e
Octavio Solis <octavio.solis1@gmail.com>
2025-06-21 22:43:09 -0600

    modificación del logo y color de las entradas

--- View/viewTask.py
+++ View/viewTask.py
@@ -8,26 +8,25 @@ class TodoView:
         ctk.set_appearance_mode("dark")  
         ctk.set_default_color_theme("blue")
         self.window = window
-        self.window.geometry("650x650")
-        self.window.title("To-do List Octaviosx")
+        self.window.geometry("800x800")
+        self.window.title("To-do List Octavio")
         self.window.iconbitmap("src/icons/todo_list.ico")
         self.window.configure(fg_color=ctk.ThemeManager.theme["CTk"]["fg_color"])
         fuente_global = tkFont.Font(family="Roboto")
         self.window.option_add("*Font", fuente_global)
         
         image = Image.open("D:/universidad/to-do-list/todo_list/src/icons/hybridge_log.png")
-        image = image.resize((400, 400))  # Ajusta tamaño aquí
-        self.ctk_image = ctk.CTkImage(light_image=image, dark_image=image)
+        self.ctk_image = ctk.CTkImage(light_image=image, dark_image=image, size=(110, 135))
         self.label_img = ctk.CTkLabel(window, image=self.ctk_image, text="")
-        self.label_img.pack(pady=10)
+        self.label_img.pack(pady=40)
         
-        self.entry = ctk.CTkEntry(window, width=200, fg_color="azure", text_color='black')
+        self.entry = ctk.CTkEntry(window, width=200, fg_color="#5c5c5c", text_color='#f0f0f0')
         self.entry.pack(pady=10)
         
         self.buttonAdd = ctk.CTkButton(window, text='Agregar tarea', corner_radius=15, fg_color="DodgerBlue2", text_color="white")
         self.buttonAdd.pack()
         
-        self.list = tk.Listbox(window, width=50, height=10, bg="gray98")
+        self.list = tk.Listbox(window, width=50, height=10, bg="#5c5c5c")
         self.list.pack(pady=10)
         
         self.buttonDelete = ctk.CTkButton(window, text="Eliminar tarea seleccionada", corner_radius=15, fg_color="DodgerBlue2", text_color="white")
